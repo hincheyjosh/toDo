55acf95b9066cef0a00fe678dea148b01de71eb9
Joshua Hinchey <hincheyjosh@gmail.com>
2022-01-17 18:59:35 -0600

    added table for task view

--- src/domStuff.js
+++ src/domStuff.js
@@ -9,10 +9,50 @@ function updateProjectList() {
     const list = document.querySelector(".projects");
     const listItems = projects
         .map((project) => {
-            return `<li data-name="${project.title}">${project.title}</li>`;
+            return `<li id="projectListItem" data-name="${project.title}">${project.title}</li>`;
         })
         .join("");
     list.innerHTML = listItems;
+
+    const projectList = document.querySelectorAll("#projectListItem");
+    projectList.forEach((project) => {
+        project.addEventListener("click", changeCurrentProject);
+    });
+}
+
+function updateTaskList() {
+    const tableArea = document.querySelector(".tableArea");
+    const table = document.createElement("table");
+    table.className = "tasksTable";
+    table.innerHTML += `<thead>
+                        <tr>
+                            <th></th>
+                            <th>Task</th>
+                            <th>Description</th>
+                            <th>Due Date</th>
+                            <th>Priority</th>
+                        </tr>
+                        </thead>
+                        <tbody>`;
+    const tasksToAdd = currentProject
+        .getTasks()
+        .map((task) => {
+            return `<tr>
+            <td><input type="checkbox" id="checkbox"></input></td>
+                        <td>${task.name}</td>
+                        <td>${task.description}</td>
+                        <td>${task.dueDate}</td>
+                        <td>${task.priority}</td>
+                    </tr>`;
+        })
+        .join("");
+    table.innerHTML += tasksToAdd + `</tbody>`;
+    tableArea.appendChild(table);
+
+    const checkBoxes = document.querySelectorAll("#checkbox");
+    checkBoxes.forEach((box) => {
+        box.addEventListener("change", strikeThrough);
+    });
 }
 
 function showForm() {
@@ -23,4 +63,21 @@ function hideForm() {
     document.querySelector(".taskAreaForm").style.display = "none";
 }
 
-module.exports = { toggleForm, updateProjectList, showForm, hideForm };
+function strikeThrough() {
+    this.parentElement.parentElement.classList.toggle("checked");
+}
+
+function changeCurrentProject() {
+    currentProject = this.dataset.name;
+    console.log(currentProject);
+}
+
+module.exports = {
+    toggleForm,
+    updateProjectList,
+    showForm,
+    hideForm,
+    updateTaskList,
+    strikeThrough,
+    changeCurrentProject,
+};
--- src/index.js
+++ src/index.js
@@ -6,16 +6,32 @@ import * as Project from "./classes.js";
 document.getElementById("todoIcon").src = todoIcon;
 
 global.projects = [];
-
-const project1 = Project.createProject("Coding");
-console.log(project1);
-project1.addTask("Practice", "5/25/2020", "get better", "High");
+const project1 = Project.createProject("My First Project");
+project1.addTask(
+    "Practice Coding",
+    "5/25/2020",
+    "get better at doing almost everything",
+    "High"
+);
 projects.push(project1);
+global.currentProject = projects[0];
+
+DOM.updateProjectList();
+DOM.updateTaskList();
+
+const projectForm = document.querySelector(".newProjectForm");
+const checkBoxes = document.querySelectorAll("#checkbox");
+
+projectForm.addEventListener("submit", function () {
+    const projectTitle = projectForm["newProjectTitle"].value;
+    const newProject = Project.createProject(projectTitle);
+    projects.push(newProject);
+    projectForm["newProjectTitle"].value = "";
+    DOM.updateProjectList();
+});
 
 document.querySelector(".addBtn").onclick = DOM.toggleForm;
 document.querySelector(".createBtn").onclick = DOM.toggleForm;
 document.getElementById("submit").onclick = DOM.hideForm;
 document.getElementById("addTaskBtn").onclick = DOM.showForm;
 document.getElementById("closeLink").onclick = DOM.hideForm;
-
-DOM.updateProjectList();
